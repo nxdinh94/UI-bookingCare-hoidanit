596d097c71039d0f89f7d8b769ac7da9acf7fbb9
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-09-23 10:08:19 +0700

    #48 tich hop thu vien react-slider-carousel

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
-
+import Specialty from './Section/Specialty';
 class HomePage extends Component {
 
     render() {
-        
         return (
             <div>
                 <HomeHeader />
+                <div style={{height: '80vh', width: '100%', background: 'transparent'}}></div>
+                <Specialty />
+                <div style={{height: '900px', width: '100%', background: 'red'}}></div>
             </div>
         );
     }
