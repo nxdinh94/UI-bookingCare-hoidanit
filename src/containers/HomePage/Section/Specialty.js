078bddf9b53fe1f05e70f040e2ca2446672ef0ab
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-09-29 15:02:59 +0700

    customize slider

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -6,40 +6,56 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
 let settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToShow: 4,
+    slidesToScroll: 1,
+ 
 };
+
 class Specialty extends Component {
 
     render() {
         return (
             <div className='section-specialty'>
-                <div className='specialty-content'>
-                    <Slider {...settings}>
-                        <div className='img-customize'>
-                            <h3>1</h3>
+                <div className='specialty-container'>
+                    <div className='specialty-header'>
+                        <span className='title-section'>Chuyên khoa phổ biến</span>
+                        <button className='btn-section'>Xem thêm</button>
+                    </div>
+                    <div className='specialty-body'>
+                        <Slider {...settings}>
+                        <div className='specialty-customize'>
+                            <div className='bg-image'></div>
+                            <div>Cơ sương khớp 1</div>
                         </div>
-                        <div className='img-customize'>
-                            <h3>2</h3>
+                        <div className='specialty-customize'>
+                            <div className='bg-image'></div>
+                            <div>Cơ sương khớp 1</div>
                         </div>
-                        <div className='img-customize'>
-                            <h3>3</h3>
+                        <div className='specialty-customize'>
+                           <div className='bg-image'></div>
+                            <div>Cơ sương khớp 1</div>
                         </div>
-                        <div className='img-customize'>
-                            <h3>4</h3>
+                        <div className='specialty-customize'>
+                           <div className='bg-image'></div>
+                            <div>Cơ sương khớp 1</div>
                         </div>
-                        <div className='img-customize'>
-                            <h3>5</h3>
+                        <div className='specialty-customize'>
+                           <div className='bg-image'></div>
+                            <div>Cơ sương khớp 1</div>
                         </div>
-                        <div className='img-customize'>
-                            <h3>6</h3>
+                        <div className='specialty-customize'>
+                            <div className='bg-image'></div>
+                            <div>Cơ sương khớp 1</div>
                         </div>
                 
                     </Slider>
+                    </div>
+                    
                 </div>
             </div>
         );
